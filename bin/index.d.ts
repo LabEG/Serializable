2d79ecd8b543c6140e9563c4f402d4c9573582dc
Labutin Evgeniy <labeg@mail.ru>
2018-10-27 17:57:36 +0300

    update index file

--- bin/index.d.ts
+++ bin/index.d.ts
@@ -1,2 +1,3 @@
 export { jsonProperty } from './decorators/jsonProperty';
+export { jsonIgnore } from './decorators/jsonIgnore';
 export { Serializable } from './classes/Serializable';
--- bin/index.js
+++ bin/index.js
@@ -2,5 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var jsonProperty_1 = require("./decorators/jsonProperty");
 exports.jsonProperty = jsonProperty_1.jsonProperty;
+var jsonIgnore_1 = require("./decorators/jsonIgnore");
+exports.jsonIgnore = jsonIgnore_1.jsonIgnore;
 var Serializable_1 = require("./classes/Serializable");
 exports.Serializable = Serializable_1.Serializable;
