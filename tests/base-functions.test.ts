dbaef179cc754ccf076ec881da4d021b28dc20bc
Labutin Evgeniy <labeg@mail.ru>
2020-05-16 13:14:17 +0300

    minor fixes

--- tests/base-functions.test.ts
+++ tests/base-functions.test.ts
@@ -130,39 +130,5 @@ describe("Serializable", () => {
                 assert.strictEqual(friend.name, object.friends[index].name, `friend ${index} name is not equal`);
             });
         });
-
-        it.only("deserialize must support fromJson without jsonProperty attribute", async() => {
-            const { UserJS } = await import("./models/UserJS");
-            const json = await import("./jsons/json-generator.json");
-            const [object] = json;
-
-            const user = new UserJS().fromJSON(object);
-
-            assert.isTrue(user instanceof UserJS);
-            assert.strictEqual(user.id, object.id, "id is not equal");
-            assert.strictEqual(user.index, object.index, "index is not equal");
-            assert.strictEqual(user.guid, object.guid, "guid is not equal");
-            assert.strictEqual(user.isActive, object.isActive, "isActive is not equal");
-            assert.strictEqual(user.balance, object.balance, "balance is not equal");
-            assert.strictEqual(user.picture, object.picture, "picture is not equal");
-            assert.strictEqual(user.age, object.age, "age is not equal");
-            assert.strictEqual(user.eyeColor, object.eyeColor, "eyeColor is not equal");
-            assert.strictEqual(user.name, object.name, "name is not equal");
-            assert.strictEqual(user.company, object.company, "company is not equal");
-            assert.strictEqual(user.email, object.email, "email is not equal");
-            assert.strictEqual(user.phone, object.phone, "phone is not equal");
-            assert.strictEqual(user.address, object.address, "address is not equal");
-            assert.strictEqual(user.about, object.about, "about is not equal");
-            assert.strictEqual(user.latitude, object.latitude, "latitude is not equal");
-            assert.strictEqual(user.longitude, object.longitude, "longitude is not equal");
-            assert.deepEqual(user.tags, object.tags, "tags is not equal");
-            assert.strictEqual(user.greeting, object.greeting, "greeting is not equal");
-            assert.strictEqual(user.favoriteFruit, object.favoriteFruit, "favoriteFruit is not equal");
-
-            user.friends.forEach((friend: FriendExt, index: number) => {
-                assert.strictEqual(friend.id, object.friends[index].id, `friend ${index} id is not equal`);
-                assert.strictEqual(friend.name, object.friends[index].name, `friend ${index} name is not equal`);
-            });
-        });
     });
 });
