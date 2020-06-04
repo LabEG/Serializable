bb14cbc307facf26b5979e35302cd3898652250a
Evgeniy Labutin <labeg@mail.ru>
2020-06-04 16:02:10 +0000

    disable some lints

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     extends: ["./node_modules/@labeg/code-style/.eslintrc.js"],
     rules:{
         // override here
-        "@typescript-eslint/member-ordering": "off"
+        "@typescript-eslint/member-ordering": "off",
+        "@typescript-eslint/naming-convention": "off"
     }
 };
