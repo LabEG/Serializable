f16f6fbfe6c433ee4827ac4634b12b2109b52aea
Eugene Labutin (CI/CD) <labeg@mail.ru>
2022-11-22 07:28:47 +0000

    chore: bump dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,5 +2,6 @@ module.exports = {
     extends: ["./node_modules/@labeg/code-style/.eslintrc.js"],
     rules:{
         // override here
+        "@typescript-eslint/keyword-spacing": 0 // bug, remove later
     }
 };
