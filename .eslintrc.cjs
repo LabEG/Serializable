3b44a78a6dc72bc3bc491aa086abb9ddeed1d32c
Eugene Labutin (CI/CD) <labeg@mail.ru>
2023-12-16 12:54:21 +0000

    chore: update code for new code style

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,9 @@
+"use strict"
+
 module.exports = {
     extends: ["./node_modules/@labeg/code-style/.eslintrc.js"],
+    ignorePatterns: ["node_modules/*", "dist/*"],
     rules:{
-        // override here
+        // Override here
     }
 };
