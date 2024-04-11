0e6048abcebd0a967e4b21d57b7166eaf6130572
Danila Danko <br4ch1st0chr0n3@gmail.com>
2024-04-11 19:05:37 +0300

    feat: generate multiple ts files

--- tests/generator/src/Test.hs
+++ tests/generator/src/Test.hs
@@ -1,25 +1,27 @@
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE OverloadedLabels #-}
 {-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Test where
 
 import AST (SomeClass)
 import Control.Lens ((^.))
+import Control.Monad (forM_)
 import Data.Generics.Labels ()
 import Data.Generics.Product ()
 import Data.List (intercalate)
 import Data.List.NonEmpty
-import Data.String.Interpolate (__i'L)
+import Data.String.Interpolate (i, __i'L)
 import Generate
 import Hedgehog.Gen
 
 main :: IO ()
-main = do
+main = forM_ [1 .. 3] $ \(idx :: Int) -> do
   classes <- sample (genClasses 10 (pure []))
   topClass <- sample $ genTopClass classes
   let topClassName = show $ topClass ^. #_someClass_name
-  writeFile "ts/generated.ts" (makeTS classes topClass topClassName)
+  writeFile [i|ts/generated#{idx}.ts|] (makeTS classes topClass topClassName)
 
 makeTS :: NonEmpty SomeClass -> SomeClass -> String -> String
 makeTS classes topClass topClassName =
