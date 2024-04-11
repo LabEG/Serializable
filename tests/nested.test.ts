1cb0356fdaf33a367efba24ec265d82590bcce38
Danila Danko <br4ch1st0chr0n3@gmail.com>
2024-04-11 19:04:16 +0300

    fix: use multiple test classes in the nested classes test

--- tests/nested.test.ts
+++ tests/nested.test.ts
@@ -1,17 +1,25 @@
 import {assert} from "chai";
-import {TestClass} from "./generator/ts/generated";
+import {TestClass as TestClass1} from "./generator/ts/generated1";
+import {TestClass as TestClass2} from "./generator/ts/generated2";
+import {TestClass as TestClass3} from "./generator/ts/generated2";
+import type {Serializable} from "../src";
+
+// eslint-disable-next-line func-names
+const itTestClass = function <T extends Serializable> (idx: number, TestClass: new () => T): void {
+    it("deserialized object should be equal to the original object", () => {
+        const testObject = new TestClass();
+
+        const testObjectSerialized = testObject.toJSON();
+
+        const testObjectDeserialized = (new TestClass()).fromJSON(testObjectSerialized);
+
+
+        assert.deepStrictEqual(testObject, testObjectDeserialized);
+    });
+};
 
 describe("Nested classes", () => {
-    const numberOfIterations = 3;
-    for (let iteration = 1; iteration <= numberOfIterations; iteration += 1) {
-        it("deserialized object should be equal to the original object", () => {
-            const testObject = new TestClass();
-
-            const testObjectSerialized = testObject.toJSON();
-
-            const testObjectDeserialized = TestClass.fromJSON(testObjectSerialized);
-
-            assert.deepStrictEqual(testObject, testObjectDeserialized);
-        });
-    }
+    itTestClass(1, TestClass1);
+    itTestClass(2, TestClass2);
+    itTestClass(2, TestClass3);
 });
